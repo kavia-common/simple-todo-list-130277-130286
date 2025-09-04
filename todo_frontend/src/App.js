129a0353259855153871fcc8751bcf09344d96a8
Kavia AI <production@kavia.ai>
2025-09-04 17:29:17 +0000

    CheckPoint - cgad1216aa

--- todo_frontend/src/App.js
+++ todo_frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CalendarDualMode from './components/CalendarDualMode';
 
 // PUBLIC_INTERFACE
 function App() {
@@ -42,7 +43,9 @@ function App() {
           Learn React
         </a>
       </header>
-      {/* CalendarDualMode has been removed from default render */}
+
+      {/* Render the converted calendar UI */}
+      <CalendarDualMode initialMonth="April" initialYear={2021} />
     </div>
   );
 }
